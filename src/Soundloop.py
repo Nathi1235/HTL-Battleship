c2801068199a383411d340aea25393efc6ab1aef
JoensJakobBerzellius <jonny.gebhart@gmail.com>
2023-04-20 15:38:36 +0200

    shiffanzahl wird reduziert

--- src/Soundloop.py
+++ src/Soundloop.py
@@ -9,12 +9,21 @@ import winsound
 
 def soundtrack():
     while True:
-        filename = 'Bmark3.wav'
+        filename = 'Resources\Sound\Bmark3.wav'
         winsound.PlaySound(filename, winsound.SND_FILENAME)
-        filedings = 'Ctruce3.wav'
+        filedings = 'Resources\Sound\Ctruce3.wav'
         winsound.PlaySound(filedings, winsound.SND_FILENAME)
         
         
 def start_soundtrack():
     soundtrack_thread = threading.Thread(target=soundtrack)
     soundtrack_thread.start()
+
+def oneshipleft():
+    filename= 'Resources\Sound\alarm.wav'
+    while True:
+        winsound.PlaySound(filename, winsound.SND_FILENAME)
+
+def start_oneshipleft():
+    oneshipleft_thread = threading.Thread(target=oneshipleft)
+    oneshipleft_thread.start()
